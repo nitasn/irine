59f7c0f262dbc0ba0c3771bb4eded2d360e71c7e
mac <mac@nitsan15.lan>
2022-12-26 20:16:44 +0200

    tryna see if push auto rebuilds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  gap: 15px;
+`;
+
+const Headline = styled.h1`
+  font-size: 2rem;
+`;
+
+const DateInput = styled.input.attrs({ type: "date" })`
+  width: 250px;
+  height: 30px;
+  margin-bottom: 30px;
+  margin-top: 50px;
+  padding: 15px;
+  background-color: white;
+  color: black;
+  border: 2px solid #677788;
+  border-radius: 5;
+  font-size: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Headline>Welcome.</Headline>
+      <DateInput />
+    </Container>
   );
 }
 
